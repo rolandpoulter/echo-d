674754f2e06efc02f8fb4dc52598b8dfae426a0c
roland <rolandpoulter@gmail.com>
2024-02-03 15:56:15 -0800

    fix puppeteer in CI

--- ecmascript/jest-puppeteer.config.cjs
+++ ecmascript/jest-puppeteer.config.cjs
@@ -1,7 +1,24 @@
 /** @type {import('jest-environment-puppeteer').JestPuppeteerConfig} */
+
+const CI = Boolean(process.env.CI || false);
+
+const launch = {
+  dumpio: true,
+  headless: true,
+};
+
+if (CI) {
+  launch.headless = true;
+  launch.executablePath = '/usr/bin/google-chrome-stable'; // '/usr/bin/chromium-browser';
+  launch.args = [
+    '--ignore-certificate-errors',
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    '--disable-accelerated-2d-canvas',
+    '--disable-gpu'
+  ];
+}
+
 module.exports = {
-  launch: {
-    dumpio: true,
-    headless: true,
-  },
+  launch,
 };
